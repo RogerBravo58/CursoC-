92b28e0a071caf43e6af3848f3974a16eb239384
Rogerio Coelho <rogerio.coelho@hotmail.com>
2023-12-06 08:00:14 -0300

    Versao - 06-12-23

--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 
 using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturasDeControle;
+using CursoCSharp.ClassesEMetodos;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                // FUNDAMENTOS
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar },
                 {"Comentarios - Fundamentos", Comentarios.Executar},
                 {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
@@ -16,7 +19,38 @@ namespace CursoCSharp {
                 {"Lendo Dados - Fundamentos", LendoDados.Executar},
                 {"Formatando Números - Fundamentos", FormatandoNumero.Executar},
                 {"Conversões - Fundamentos",  Conversoes.Executar},
-                {"Operadores Aritméticos - Fundamentos",  OperadoresAritmeticos.Executar},
+                {"Operadores Aritméticos - Fundamentos", OperadoresAritmeticos.Executar},
+                {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar},
+                {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
+                {"Operadores Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
+                {"Operadoes Unários - Fundamentos", OperadoesUnarios.Executar},
+                {"Operador Ternário - Fundamentos", OperadorTernario.Executar},
+
+                // ESTRUTURAS DE CONTROLE
+                {"Estruturas IF - Estruturas De Controle", EstruturaIF.Executar},
+                {"Estrutura If/Else - Estruturas De Controle", EstruturaIfElse.Executar},
+                {"Estrutura If/Else/IF - Estruturas De Controle", EstruturaIfElseIF.Executar},
+                {"Estrutura Switch - Estruturas De Controle", EstruturaSwitch.Executar},
+                {"Estrutura While - Estruturas De Controle", EstruturaWhile.Executar},
+                {"Estrutura DoWhile - Estruturas De Controle", EstruturaDoWhile.Executar},
+                {"Estrutura For - Estruturas De Controle", EstruturaFor.Executar},
+                {"Estrutura Foreach - Estruturas De Controle", EstruturaForEach.Executar},
+                {"Usando Break - Estruturas De Controle", UsandoBreak.Executar},
+                {"Usando Continue - Estruturas De Controle", UsandoContinue.Executar},
+                
+                // ESTRUTURAS DE CONTROLE
+                {"Membros - Classes e Métodos", Membros.Executar},
+                {"Construtores - Classes e Métodos", Construtores.Executar},
+                {"Métodos Com Retorno - Classes e Métodos", MetodosComRetorno.Executar},
+                {"Métodos Estáticos - Classes e Métodos", MetodosEstaticos.Executar},
+                {"Atributos Estáticos - Classes e Métodos", AtributosEstaticos.Executar},
+                {"Desafio Atributo - Classes e Métodos", DesafioAtributo.Executar},
+                {"Params - Classes e Métodos", Params.Executar},
+                {"Parametros Nomeados - Classes e Métodos", ParametrosNomeados.Executar},
+                {"Get Ser - Classes e Métodos", GetSet.Executar},
+                {"Props - Classes e Métodos", Props.Executar},
+                {"Readonly - Classes e Métodos", Readonly.Executar},
+                {"Exemplo Enum - Classes e Métodos", ExemploEnum.Executar},
             });
 
             central.SelecionarEExecutar();
